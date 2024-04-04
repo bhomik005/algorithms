cc2e08b5ab522fd7df7d0e7fc2b8ab6f8f969110
bhomik005 <bhomik005@gmail.com>
2024-04-05 00:06:55 +1100

    init sorting algorithms

---
+++ sorting_algorithms/bubble_sort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+void swap(int i, int j, vector<int>& array) {
+  int temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+vector<int> bubbleSort(vector<int> array) {
+  int counter = 0;
+  bool isSorted = false;
+  for(int i = 0; i < array.size(); ++i) {
+    isSorted = true;
+    for(int j = 0; j < array.size() - 1 - counter; ++j) {
+      if(array[j] > array[j+1]) {
+        swap(j, j + 1, array);
+        isSorted = false;
+      }
+    }
+    if(isSorted == true) break;
+    counter += 1;
+  }
+  return array;
+}
+
+
+int main() {
+  vector<int> array {5,4,3,2,1};
+  vector<int> sortedArray = bubbleSort(array);
+  for(int num : sortedArray) cout << num << " ";
+  cout << "\n";  
+  return 0;
+}
